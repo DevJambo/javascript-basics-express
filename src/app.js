ca13a122a835e2c1b48b74d07350c828de3a1557
James Goodman <jamesgoodman206@gmail.com>
2023-01-05 22:01:36 +0000

    first 3 array tests passed

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const {
 } = require('./lib/strings');
 const { add, subtract, multiply, divide, remainder } = require('./lib/numbers');
 const { negate, truthiness, isOdd, startsWith } = require('./lib/booleans');
+const { getNthElement, arrayToCSVString, addToArray, addToArray2 } = require('./lib/arrays');
 
 const app = express();
 
@@ -136,5 +137,21 @@ app.get('/booleans/:string/starts-with/:character', (req, res) => {
   } else {
     res.status(200).json({ result: startsWith(b, a) });
   }
+});
+
+app.post('/arrays/element-at-index/:index', (req, res) => {
+  const { array } = req.body;
+  const { index } = req.params;
+  res.status(200).json({ result: getNthElement(index, array) });
+});
+
+app.post('/arrays/to-string', (req, res) => {
+  const { array } = req.body;
+  res.status(200).json({ result: arrayToCSVString(array) });
+});
+
+app.post('/arrays/append', (req, res) => {
+  const { array, value } = req.body;
+  res.status(200).json({ result: addToArray2(value, array) });
 });
 module.exports = app;
